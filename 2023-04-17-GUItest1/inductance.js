e85c6855fd07cf1b7a4c7cbad7159445ba407e2b
dbnyu <65036353+dbnyu@users.noreply.github.com>
2023-04-26 16:48:00 -0400

    new file

---
+++ 2023-04-17-GUItest1/inductance.js
@@ -0,0 +1,15 @@
+/* BirdcageBuilder
+ * 
+ * Compute self- and mutual-inductance for legs and endrings.
+ *
+ * Doug Brantner
+ * 26 April 2023
+ * NYU Langone Health
+ * cai2r.net
+ *
+ * Based on Android implementation of BirdcageBuilder by Giuseppe Carluccio & Christopher Collins.
+ */
+
+
+
+
