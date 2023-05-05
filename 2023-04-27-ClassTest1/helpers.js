5c77476fbc53f9b9d603c4b5de0429f1c986a237
dbnyu <65036353+dbnyu@users.noreply.github.com>
2023-05-05 17:05:36 -0400

    add new modulo workaround function

--- 2023-04-27-ClassTest1/helpers.js
+++ 2023-04-27-ClassTest1/helpers.js
@@ -10,6 +10,8 @@
  */
 
 
+/*** Unit Conversions ***/
+/* The constants can be used directly, or called in functions */
 
 // Multiplicative Constants (multiply to acheive desired result)
 var CM2M = 1e-2;	// centimeters to meters
@@ -26,4 +28,26 @@ function mhz2hz(mhz) {
 	// Convert MHz to Hertz
 
 	return MHZ2HZ * mhz;
+}
+
+
+
+
+
+
+/*** Math Functions ***/
+
+
+function mod(n, d) {
+	/* Modulo operator
+	 * Implementation Details:
+	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
+	 *
+	 * The % operator is NOT the same in Javascript as in other languages;
+	 * the above reference gives this implementation for the expected behavior.
+	 */
+
+	// TODO UNIT TESTS - compare with actual Java output to confirm expected behavior!!!
+
+	return ((n % d) + d) % d;
 }
