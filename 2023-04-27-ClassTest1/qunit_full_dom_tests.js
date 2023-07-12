3ffa117b7288c463694852ccee10ce10b5ecd368
dbnyu <65036353+dbnyu@users.noreply.github.com>
2023-07-12 13:20:17 -0400

    copy plotly test for freqshift and add placeholder for full workflow GUI tests

---
+++ 2023-04-27-ClassTest1/qunit_full_dom_tests.js
@@ -0,0 +1,9 @@
+// TODO placeholder
+// Try to use QUnit to manipulate DOM inputs (simulate user input)
+// with 2002 Paper test cases
+// and make sure the final output is correct
+//
+// ie all other qunit tests bypass the entire GUI & unit conversion layer
+//
+// so this would be a full-DOM workflow test
+// ie. especially for odd cases like Bandpass predcap and rectangular/tubular endring inputs
